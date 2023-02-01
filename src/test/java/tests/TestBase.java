729d0eae95498b586ccf959c64529b8e594b1e3a
ILYA KOPYTOV <kopytov.ilya.rus@mail.ru>
2023-02-01 12:01:22 +0300

    Properties added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,10 +17,10 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = "chrome";
-        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = System.getProperty("resolution", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "100.0");
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
