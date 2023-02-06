789ba2895cf155cbc525837f4b3a046b6b9685fc
ILYA KOPYTOV <kopytov.ilya.rus@mail.ru>
2023-02-06 14:33:52 +0300

    Properties added

--- src/test/java/tests/StudentRegistrationForm.java
+++ src/test/java/tests/StudentRegistrationForm.java
@@ -2,6 +2,7 @@ package tests;
 
 import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -9,6 +10,7 @@ import static io.qameta.allure.Allure.step;
 public class StudentRegistrationForm extends TestBase {
 
     @Test
+    @Tag("Positive")
     @Feature("Форма анкетирования")
     @Story("Заполнение формы")
     @Owner("Kopytov-Ilya")
@@ -122,6 +124,7 @@ public class StudentRegistrationForm extends TestBase {
 
 
     @Test
+    @Tag("Negative")
     @Feature("Форма анкетирования")
     @Story("Заполнение формы")
     @Owner("Kopytov-Ilya")
